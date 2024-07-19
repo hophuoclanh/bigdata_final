242156ae72d44d632a6d17e9c9dc2aac439f6d1c
hophuoclanh <110510610+hophuoclanh@users.noreply.github.com>
2024-07-19 16:54:56 +0700

    Create mobile_price_streaming.py

---
+++ mobile_price_streaming.py
@@ -0,0 +1,17 @@
+import csv
+import json
+import time
+import random
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers="192.168.80.83:9092",
+                         value_serializer=lambda m: json.dumps(m).encode('utf-8'))
+
+with open('test.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        try:
+            producer.send("mobile_price", row)
+            time.sleep(random.randint(7, 20))
+        except Exception as e:
+            print(f"Error: {e}")
